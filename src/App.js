93f216c1655ad4bf89e5fc0ce3bcbf1b81a8a3af
aaudino <a.audino95@gmail.com>
2022-11-10 11:00:35 +0100

    ComponentDidMount inc async await & error handling

--- src/App.js
+++ src/App.js
@@ -7,54 +7,56 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: { firstname: "Anthony", lastname: "Audino" },
-      company: "WU",
+      monsters: [],
     };
+  }
+  //Component did mount ist eine Lifecycle Method
+  //componentdidmount Ist eine Methode die in Kraft tritt wenn die Componente das erste mal in den DOM integriert wird.
+  async componentDidMount() {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      if (!response.ok) {
+        throw Error("Unable to fetch");
+      }
+      const data = await response.json();
+
+      this.setState(() => {
+        return { monsters: data };
+      });
+    } catch (error) {
+      this.setState(() => {
+        const data = {
+          name: error.message,
+          id: 404,
+        };
+        return { monsters: [data] };
+      });
+    }
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi there, {this.state.name.firstname} {this.state.name.lastname} -
-            student of {this.state.company}{" "}
-          </p>
-          {/* betreffend Eventhandler:wir benötigen hier die setState methode, da diese React darüber informiert, dass das State Object zu einer neuen Speicherreferenz zeigt - das animiert react dazu, den DOM neu zu rendern würden wir einfach bei der Onclick Methode this.state.name ändern, dann löst das kein rerendern des DOMS aus, weil wir ja die Speicherreferenz nicht anrühren*/}
-          <button
-            onClick={() => {
-              this.setState(
-                //Um die die kombination aus Setstate und bspw console.log() synchron verlaufen zu lassen, werden die änderungen an dem State Object in eine seperate callback function gesteckt, welche das state object und die props als optionale arguments verwenden kann
-                // Das Console.log wird in eine weitere Callback function gesteckt die NUR dann ausgeführt wird, wenn die setState callback function beendet wurde
-                (state, props) => {
-                  return {
-                    name: { firstname: "Antonio", lastname: "Audino" },
-                  };
-                },
-                () => {
-                  console.log(this.state);
-                }
-              );
-            }}
-          >
-            {" "}
-            Click here to change name{" "}
-          </button>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            // Die Id muss hinzugefügt werden beim mappen - damit react weiß welche Elemente bei einer Veränderung neu gerendert werden müssen.
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
 }
 
 export default App;
+
+//Reihenfolge:
+//1. contructor und initalize the state
+//2. Danach läuft Render() und kreiert mehr oder weniger ein Template
+//3 Das Template wird von Component did mount befüllt
+
+// Das ist auch ganz logisch weil die komponnente kann nämlich nur dann mounten wenn der dom generiert wird
+// Component.mount veranlasst dann React wiederum dazu, den Dom wieder neu zu rendern
