ebcbcdd79d4af50a6df80398c250a710ac35baf1
aaudino <a.audino95@gmail.com>
2022-11-10 13:23:26 +0100

    refactoring & destructuring

--- src/App.js
+++ src/App.js
@@ -36,10 +36,26 @@ class App extends Component {
       });
     }
   }
+
+  //Wir lagern die Onsearch funktion auf eine eigene Function aus -> wesentlich perfromanter als eine anonyme function wie unten
+  //Diese Methode ist an unsere Klasse gebunden
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+    this.setState(() => {
+      //Wenn wir eine Variable haben die gleich wie eine Property heißt, dann können wir diesen shortcut verwenden
+      return {
+        searchField,
+      };
+    });
+  };
   //Das Problem bei der Basic Version der Search war: Wir haben Monsters mit filteredmonsters gleichgesetzt. und damit war es nicht mehr möglich "den Ursprungszustand von monsters herzustellen. Wir haben const filteredMonsters aus dem vorherigen Scope in einen Höheren Scope geschoben, damit wir das dann Mappen können. und aus dem Searchfield eine Property gemacht, damit filteredMonsters darauf zugreifen kann
   render() {
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLowerCase().includes(this.state.searchField);
+    // Destructuring damit wir das "this" streichen können
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
     });
     return (
       <div className="App">
@@ -47,15 +63,7 @@ class App extends Component {
           className="search-box"
           type="search"
           placeholder="search monsters"
-          onChange={(event) => {
-            const searchField = event.target.value.toLowerCase();
-            this.setState(() => {
-              //Wenn wir eine Variable haben die gleich wie eine Property heißt, dann können wir diesen shortcut verwenden
-              return {
-                searchField,
-              };
-            });
-          }}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
