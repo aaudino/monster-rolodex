a2353b5952ce6cbb4cd6ebd58e06130f1ad6a76d
aaudino <a.audino95@gmail.com>
2022-11-11 09:48:02 +0100

    implemented search box styling

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,11 +1,14 @@
 import { Component } from "react";
+//Andere Komponenten können auch auf diese Style file zugreifen. Das liegt daran dass css gebundelt wird.Die Inputs werden aber auf Komponente ebene integriert, weil es einfach übersichtlicher ist
+import "./search-box.styles.css";
 
 class SearchBox extends Component {
   //Die Komponente muss abstrahiert werden - der Type kann so behalten werden wie er ist
+  //Das gilt auch für die CSS Classes
   render() {
     return (
       <input
-        className={this.props.className}
+        className={` search-box ${this.props.className}`}
         type="search"
         placeholder={this.props.placeholder}
         onChange={this.props.onChangeHandler}
